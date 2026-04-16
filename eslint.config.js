faa14e254d5304320d009ebd5ae8397ad2b8299e
Minna Ronka <minna.ronka@gmail.com>
2026-04-16 13:21:01 +0300

    Update dependencies package-lock.json

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,13 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    'node_modules',
+    'build',
+    'coverage',
+    'dev'
+  ]),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
